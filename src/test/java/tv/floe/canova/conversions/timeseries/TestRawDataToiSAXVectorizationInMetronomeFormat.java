2051cb80c8f52584d0343593a9274088b95d19e5
Josh Patterson <jpatterson@floe.tv>
2014-06-17 13:15:18 -0400

    added some mock unit tests for the timeseries vectorizers

---
+++ src/test/java/tv/floe/canova/conversions/timeseries/TestRawDataToiSAXVectorizationInMetronomeFormat.java
@@ -0,0 +1,14 @@
+package tv.floe.canova.conversions.timeseries;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestRawDataToiSAXVectorizationInMetronomeFormat {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
