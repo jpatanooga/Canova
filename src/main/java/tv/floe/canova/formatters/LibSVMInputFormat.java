4614c9976dd319de6e72047b1f3d4aa48145bd22
MJK <mjk@emmjaykay.org>
2014-09-02 15:01:19 -0700

    libSVM input format OK

--- src/main/java/tv/floe/canova/formatters/LibSVMInputFormat.java
+++ src/main/java/tv/floe/canova/formatters/LibSVMInputFormat.java
@@ -1,39 +1,92 @@
 package tv.floe.canova.formatters;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.concurrent.ConcurrentSkipListMap;
 
 /**
  * Created by mjk on 9/1/14.
  */
-public class LibSVMInputFormat extends BaseInputFormat<String> {
-    private static final String SPACE = new String(" ");
+public abstract class LibSVMInputFormat<T> extends BaseInputFormat<T> {
+
+    private static final String DELIM_NEWLINE  = new String("\n");
+    private static final String DELIM_COLON  = new String(":");
+    private static final String DELIM_BAR  = new String("|");
+    private static final String SPACE  = new String(" ");
 
     @Override
-    public String read(InputStream is, String delim) throws IOException {
-        String ret = new String();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        String l;
-        while ( (l = reader.readLine()) != null ) {
-            String[] parts = l.split(SPACE);
-            for (String sbsr: parts) {
-                String[] feature = sbsr.split(":");
-                int index = Integer.parseInt(feature[0]);
-                double val = Double.parseDouble(feature[1]);
-            }
-        }
-        return null;
-    }
-    public String read(InputStream is) throws IOException {
+    public abstract T read(InputStream is, String delim) throws IOException;
+    public T read(InputStream is) throws IOException {
         return read(is, SPACE);
     }
 
-    @Override
-    public String read(String is, String delim) throws IOException {
-        return null;
+    /**
+     * Returns a ConcurrentSkpListMap of a libSVM entry
+     * @param is
+     * @param recordDelim -- usually a new line, here you can specify
+     * @param splitDelim -- for libSVM it's a colon but you can specify here
+     * @return
+     * @throws IOException
+     */
+    protected ConcurrentSkipListMap parseRead(InputStream is, String recordDelim, String splitDelim) throws IOException {
+        ConcurrentSkipListMap ret = new ConcurrentSkipListMap();
+
+        String l = readLine(recordDelim,is);
+
+        String[] parts = l.split(SPACE);
+
+        int classIndex = Integer.parseInt(parts[0]);
+        ret.put(0, classIndex);
+
+        for (int x = 1; x < parts.length; x++) {
+            String[] feature = parts[x].split(splitDelim);
+            int index = Integer.parseInt(feature[0]);
+            double val = Double.parseDouble(feature[1]);
+            ret.put(index,val);
+        }
+        return ret;
+    }
+    protected ConcurrentSkipListMap parseRead(InputStream is) throws IOException {
+        return parseRead(is,DELIM_NEWLINE, DELIM_COLON);
+    }
+
+    /**
+     * Read inputstream until delim is encountered; single byte character for now.
+     * @param delim -- a single byte character that demarcates the stream
+     * @param is
+     * @return
+     * @throws IOException
+     */
+    protected String readLine(String delim, InputStream is) throws IOException {
+        String ret = new String();
+        String everything = new String(".*");
+        int c;
+        while ( (c = is.read()) != -1 ) {
+            char ch = (char) c;
+            if (ch == (delim.charAt(0))) {
+                break;
+            }
+            ret += new String(String.valueOf(ch));
+        }
+        return ret;
     }
 
     @Override
-    public String read(File file, String delim) throws IOException {
-        return null;
+    public abstract T read(String is, String delim) throws IOException;
+    public  T read(String is) throws IOException {
+        return read(is, COMMA);
     }
+
+
+    @Override
+    public abstract T read(File file, String delim) throws IOException;
+    public T read(File file) throws IOException {
+        return read(file, COMMA);
+    }
+    protected ConcurrentSkipListMap fileReader(FileInputStream file, String recordDelim, String splitDelim) throws IOException {
+        return parseRead(file, recordDelim, splitDelim);
+    }
+
 }
